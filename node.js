88892683b452e8730a062b78d8cbe4d6a8f6ea80
root <luis@mendoza.edu.mx>
2020-08-13 22:06:09 +0000

    agregando puerto

--- node.js
+++ node.js
@@ -1,1 +1,7 @@
-console.log ("hola mundo ci/cd usando GithubActions");
+const express = require ('express')
+const app=express();
+app.get ('/', (req,res)=>{
+res.send({msg:"mi primera prueba"});
+
+});
+app.listen(5000, ()=> console.log ("servidor corriendo"));
